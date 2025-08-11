daebf3a864a0500db5cc41d8dd007d975c007e8e
pushpendra-student <singhpushpendra20022003@gmail.com>
2025-08-11 21:47:11 +0530

    Creating InventoryFilter to Customize Products

--- store/admin.py
+++ store/admin.py
@@ -1,9 +1,24 @@
 from django.contrib import admin
 from . import models
-from django.db.models import Count
+from django.db.models import Count, QuerySet
 from django.utils.html import format_html, urlencode
 from django.urls import reverse
 
+
+
+class InventoryFilter(admin.SimpleListFilter):
+    
+    title = 'inventory'
+    parameter_name = 'inventory'
+        
+    def lookups(self, request, model_admin):
+        return [
+            ('<10','Low')
+        ]
+        
+    def queryset(self, request, queryset : QuerySet):
+        if self.value() == '<10':
+            return queryset.filter(inventory__lt=10)
 
 @admin.register(models.Collection)
 class CollectionAdmin(admin.ModelAdmin):
@@ -31,6 +46,7 @@ class CollectionAdmin(admin.ModelAdmin):
 class ProductAdmin(admin.ModelAdmin):
     list_display = ['title', 'unit_price', 'inventory_status', 'collection']
     list_editable = ['unit_price']
+    list_filter = ['collection', 'last_update', InventoryFilter]
     list_per_page = 10
     list_select_related = ['collection']
     
@@ -45,8 +61,9 @@ class ProductAdmin(admin.ModelAdmin):
 class CustomerAdmin(admin.ModelAdmin):
     list_display = ['first_name','last_name','membership']
     list_editable = ['membership']
-    ordering = ['first_name', 'last_name']
     list_per_page = 10    
+    ordering = ['first_name', 'last_name']
+    search_fields = ['first_name__istartswith','last_name__istartswith']
 
 
 @admin.register(models.Order)
