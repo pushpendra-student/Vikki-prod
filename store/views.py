5997fbda61dfb745b74785e34aa826763fbd7ddc
pushpendra-student <singhpushpendra20022003@gmail.com>
2025-08-21 17:23:02 +0530

    Creating filters.py to customize own filters and also give filters button in api backend

--- store/views.py
+++ store/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import get_object_or_404
+from store.filters import ProductFilter
 from store.models import Product, Collection, Review
 from .serializers import ProductSerializer, CollectionSerializer, ReviewSerializer
 from rest_framework.response import Response
@@ -12,7 +13,7 @@ class ProductViewSet(ModelViewSet):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
     filter_backends = [DjangoFilterBackend]
-    filterset_fields = ['collection_id']
+    filterset_class = ProductFilter
 
     def get_serializer_context(self):
         return {'request': self.request}
