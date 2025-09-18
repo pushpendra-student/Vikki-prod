7507eb3f8d427a1a456a359ecb3983ce9ae82def
pushpendra-student <singhpushpendra20022003@gmail.com>
2025-09-18 21:09:41 +0530

    ....

--- storefront/wsgi.py
+++ storefront/wsgi.py
@@ -8,9 +8,11 @@ https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
 """
 
 import os
-
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'storefront.settings.dev')
+os.environ.setdefault(
+    'DJANGO_SETTINGS_MODULE',
+    os.environ.get('DJANGO_SETTINGS_MODULE', 'storefront.settings.dev')
+)
 
 application = get_wsgi_application()
